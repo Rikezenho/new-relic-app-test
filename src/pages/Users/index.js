222e12b72eb811e66b3dca557f54856d904505be
Henrique Kuwai <rikezenho@gmail.com>
2021-05-27 02:18:49 -0300

    setCustomAttribute

--- src/pages/Users/index.js
+++ src/pages/Users/index.js
@@ -4,7 +4,7 @@ export const Users = (props) => {
     React.useEffect(() => {
         window.newrelic = window.newrelic || {};
         if (window.newrelic.interaction) {
-            window.newrelic.interaction().setAttribute('username', 'Usuário Teste')
+            window.newrelic.setCustomAttribute('username', `Usuário Teste ${Math.floor(Math.random() * 100) + 1}`)
         } else {
             console.log('New Relic interaction not found')
         }
